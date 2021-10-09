5ba1c728bae223ec566c10d353253f43d1d35c2a
Onni Salminen <o.j.salminen@gmail.com>
2021-10-09 11:52:48 +0300

    Initial go testing

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func splitter(r rune) bool {
+	return r == '+' || r == '-'
+}
+
+func main() {
+	str := "    5     *    X   ^0   + 4 *  X^ 1 -9.3*X^2   =  1* X ^ 0"
+
+	s := strings.Join(strings.Fields(str), "")
+	fmt.Println(s)
+	content := []byte(s)
+	re := regexp.MustCompile("\\^").FindAllIndex(content, -1)
+	fmt.Println("POWERS:", re)
+	for i, val := range re {
+		fmt.Println("INDEX:", i, "strIndex 0:", string(s[val[0]]), "strIndex 1:", string(s[val[1]]))
+	}
+	// for i, strIndex := range re {
+	// 	fmt.Println("INDEX:", i, "strIndex:", string(s[strIndex]), "strIndex + 1:", string(s[strIndex]+1))
+	// }
+	// powers := strings.Index(s, "^")
+	// fmt.Println("POWERS:", powers)
+	leftRight := strings.Split(s, "=")
+	fmt.Println("leftRight:", leftRight)
+	splitRight := strings.FieldsFunc(leftRight[0], splitter)
+	fmt.Println("right side:", splitRight)
+	splitLeft := strings.FieldsFunc(leftRight[1], splitter)
+	fmt.Println("left side:", splitLeft)
+	fmt.Println(s[len(s)-1])
+}
