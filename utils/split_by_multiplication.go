f9055be474add953a85ac0ef8b788d9c3b9c85d7
Onni Salminen <o.j.salminen@gmail.com>
2021-11-23 15:43:05 +0200

    Revert split by multiplication to only work with *

--- utils/split_by_multiplication.go
+++ utils/split_by_multiplication.go
@@ -6,38 +6,41 @@
 /*   By: osalmine <osalmine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 23:48:04 by osalmine          #+#    #+#             */
-/*   Updated: 2021/11/23 13:48:42 by osalmine         ###   ########.fr       */
+/*   Updated: 2021/11/23 15:15:52 by osalmine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package utils
 
 import (
-	"regexp"
 	"strings"
 )
 
-func splitByMult(r rune) bool {
-	return r == '*' || r == 'x' || r == 'X'
-}
+// func splitByMult(r rune) bool {
+// 	return r == '*' || r == 'x' || r == 'X'
+// }
 
 func SplitByMultiplication(input string) []string {
-	var output []string
-	if !regexp.MustCompile(`[0-9]+`).MatchString(input) || regexp.MustCompile(`[0-9]+$`).MatchString(input) {
-		return []string{input}
-	}
-	for {
-		if j := strings.LastIndexFunc(input, splitByMult); j >= 0 {
-			if input[j:] != "*" {
-				output = append(output, input[j:])
-			}
-			input = input[:j]
-		} else {
-			if len(input) > 0 {
-				output = append(output, input)
-			}
-			break
-		}
-	}
+	// var output []string
+	// fmt.Println("input", input)
+	// fmt.Println("!regexp.MustCompile(`[0-9]+`).MatchString(input)", !regexp.MustCompile(`[0-9]+`).MatchString(input))
+	// fmt.Println("regexp.MustCompile(`[0-9]+$`).MatchString(input)", regexp.MustCompile(`[0-9]+$`).MatchString(input))
+	// if !regexp.MustCompile(`[0-9]+`).MatchString(input) || regexp.MustCompile(`[0-9]+$`).MatchString(input) {
+	// 	return []string{input}
+	// }
+	// for {
+	// 	if j := strings.LastIndexFunc(input, splitByMult); j >= 0 {
+	// 		if input[j:] != "*" {
+	// 			output = append(output, input[j:])
+	// 		}
+	// 		input = input[:j]
+	// 	} else {
+	// 		if len(input) > 0 {
+	// 			output = append(output, input)
+	// 		}
+	// 		break
+	// 	}
+	// }
+	output := strings.Split(input, "*")
 	return output
 }
