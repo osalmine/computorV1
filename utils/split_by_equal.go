bad60f0cc0660e8b38a74d482864ec6293710a0d
Onni Salminen <o.j.salminen@gmail.com>
2021-11-23 16:13:19 +0200

    Add check that the equation has a single =

--- utils/split_by_equal.go
+++ utils/split_by_equal.go
@@ -6,7 +6,7 @@
 /*   By: osalmine <osalmine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 23:47:57 by osalmine          #+#    #+#             */
-/*   Updated: 2021/10/30 23:47:57 by osalmine         ###   ########.fr       */
+/*   Updated: 2021/11/23 15:58:03 by osalmine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@ import "strings"
 
 func SplitByEqual(stringToSplit string) []string {
 	splittedString := strings.Split(stringToSplit, "=")
+	if len(splittedString) != 2 {
+		panic("Equation does not contain single =")
+	}
 	return splittedString
 }
