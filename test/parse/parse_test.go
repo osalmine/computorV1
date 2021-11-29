611f8d456b919d32f687688d5a2d1ab8005acfb1
Onni Salminen <o.j.salminen@gmail.com>
2021-11-29 21:07:40 +0200

    Add breaking parse test

--- test/parse/parse_test.go
+++ test/parse/parse_test.go
@@ -6,7 +6,7 @@
 /*   By: osalmine <osalmine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 13:49:52 by osalmine          #+#    #+#             */
-/*   Updated: 2021/11/23 16:59:01 by osalmine         ###   ########.fr       */
+/*   Updated: 2021/11/24 20:55:59 by osalmine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,6 +103,15 @@ func TestParse(t *testing.T) {
 			},
 		},
 		{"x^2 - x + 5 * x + 9 = 0", Options{Verbose: false, Help: false, Silent: false, Complex: false, ShowCells: false},
+			[]Cell{
+				{Coefficient: 9, Exponent: 0, Variable: false, Visual: V{CapitalX: false, DisplayExponent: false}},
+				{Coefficient: 5, Exponent: 1, Variable: true, Visual: V{CapitalX: false, DisplayExponent: false}},
+				{Coefficient: -1, Exponent: 1, Variable: true, Visual: V{CapitalX: false, DisplayExponent: false}},
+				{Coefficient: 1, Exponent: 2, Variable: true, Visual: V{CapitalX: false, DisplayExponent: true}},
+				{Coefficient: -0, Exponent: 0, Variable: false, Visual: V{CapitalX: false, DisplayExponent: false}},
+			},
+		},
+		{"4+6-5 * 4+2+8x * -2+8x^2 = x^4 - x^4", Options{Verbose: false, Help: false, Silent: false, Complex: false, ShowCells: false},
 			[]Cell{
 				{Coefficient: 9, Exponent: 0, Variable: false, Visual: V{CapitalX: false, DisplayExponent: false}},
 				{Coefficient: 5, Exponent: 1, Variable: true, Visual: V{CapitalX: false, DisplayExponent: false}},
