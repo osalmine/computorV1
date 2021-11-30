3d7d2d827c50093ebd88a5fd331a43dffe7bafdb
Onni Salminen <o.j.salminen@gmail.com>
2021-11-30 12:10:55 +0200

    Fix pow function

--- utils/pow.go
+++ utils/pow.go
@@ -6,15 +6,16 @@
 /*   By: osalmine <osalmine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 21:11:34 by osalmine          #+#    #+#             */
-/*   Updated: 2021/11/19 21:11:43 by osalmine         ###   ########.fr       */
+/*   Updated: 2021/11/30 12:10:34 by osalmine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 package utils
 
 func Pow(x float64, y int) float64 {
-	for i := 1; i < y; i++ {
-		x *= x
+	var output float64 = 1
+	for i := 1; i <= y; i++ {
+		output *= x
 	}
-	return x
+	return output
 }
